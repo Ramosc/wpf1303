8ca32345feb3d02f47d99b460ef35e7620b0da2d
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-06 11:07:53 -0500

    My wonderful story in the console log

--- Day 1/js/main.js
+++ Day 1/js/main.js
@@ -1,5 +1,26 @@
 //alert("Hello, World!");
 
-var message = "Hello World!";
-alert(message);
+//var message1 = "Hello World! 1"; // string
+//
+//var boxOfDonuts = 5.99; // number
+//
+//var wantBoxes = 5;
+//
+//var totalAmount = Math.round(boxOfDonuts * wantBoxes); //round numbers!!
+//
+//console.log("I need to buy doughnuts")
+//console.log("I need to collect:" + totalAmount + "dollars"); //concatenation
+
+var gas = 60;
+var monthly = 5;
+
+console.log("there is a guy who hates to drive")
+console.log("he hates to drive because gas is expensive; that guy is me")
+console.log("i need to pay " + "$" + gas * monthly + " every month")
+console.log("this sucks somebody do something please :(")
+
+
+
+
+
 
