474ae10ca8fe3c41fa5f1843e96656b8b8c9e709
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 10:30:37 -0500

    Expression personal 2 - Work load at full sail

--- Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -2,4 +2,6 @@ var class1 = prompt("How many hours do you spend in WPF 1303 a week?");
 var class2 = prompt("How many hours do you spend in College Math a week?");
 var work   = prompt("How many hours do you spend at work each week?");
 
+console.log("The workload at Full Sail can be very difficult at times.")
+
 
