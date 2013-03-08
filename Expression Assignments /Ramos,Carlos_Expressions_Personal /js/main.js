6be456d6368f16dbc26aa78c1d6ea6f8453ecf94
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 14:25:00 -0500

    expression personal 20 - comments added for hours portion of story

--- Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -6,10 +6,10 @@ const weekly = 40;//constant of 40 hour work week
 
 console.log("Time management is very important at a school like Full Sail. The workload at Full Sail can be very difficult at times.");//time management at full sail
 console.log("Every month your schedule will change and every week even day your schedule can change due to last minute unforeseen circumstances.");//changing of schedule
-console.log("It is very important to schedule out you time wisely especially if you have a job on top of your classes as I do.");//wisely scheduling hours  
-console.log("I have two classes a month with work and WPF is " + class1 + " hours long. College Math is " + class2 + " hours long as well." );
-console.log("I also work " + work + " hours a week and that interferes with how much time I have to finish homework assignments." );
-console.log("If you add all of that time up it equals to " + total + " and you can see why it is important to manage your time wisely.");
+console.log("It is very important to schedule out you time wisely especially if you have a job on top of your classes as I do.");//wisely scheduling hours
+console.log("I have two classes a month with work and WPF is " + class1 + " hours long. College Math is " + class2 + " hours long as well." );//class hour enter story
+console.log("I also work " + work + " hours a week and that interferes with how much time I have to finish homework assignments." );//work hours entered
+console.log("If you add all of that time up it equals to " + total + " and you can see why it is important to manage your time wisely.");//total amount of hours 
 
 if (total>=55) { //total entered by user >=55 hours
     console.log( total + " Hours is a lot of time to be taken out of the week and if continues this way there is a possibility you will burn yourself out.");//conditional answer for if
