e20b4ae4d9d3313563f99da7c44a7eabe6fa9369
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 11:01:42 -0500

    Expression personal 6 - college math hours input

--- Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -1,11 +1,12 @@
 var class1 = prompt("How many hours do you spend in WPF 1303 a week?");
-//var class2 = prompt("How many hours do you spend in College Math a week?");
+var class2 = prompt("How many hours do you spend in College Math a week?");
 //var work   = prompt("How many hours do you spend at work each week?");
 
 console.log("Time management is very important at a school like Full Sail. The workload at Full Sail can be very difficult at times.");
 console.log("Every month your schedule will change and every week even day your schedule can change due to last minute unforeseen circumstances.");
 console.log("It is very important to schedule out you time wisely especially if you have a job on top of your classes as I do.");
-console.log("I have two classes a month with work and WPF is " + class1 + " hours long a day." );
+console.log("I have two classes a month with work and WPF is " + class1 + " hours long. College Math is " + class2 + " hours long as well." ); 
+console.log("")
 
 
 
