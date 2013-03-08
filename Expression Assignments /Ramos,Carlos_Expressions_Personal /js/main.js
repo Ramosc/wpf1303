84ffc9e31ae118ca48263a7509350530b8ca0868
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 14:15:53 -0500

    expression personal 17 - comment for conditional if and else selection

--- Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -11,11 +11,11 @@ console.log("I have two classes a month with work and WPF is " + class1 + " hour
 console.log("I also work " + work + " hours a week and that interferes with how much time I have to finish homework assignments." );
 console.log("If you add all of that time up it equals to " + total + " and you can see why it is important to manage your time wisely.");
 
-if (total>=55) { //total entered by user >=55 hours 
-    console.log( total + " Hours is a lot of time to be taken out of the week and if continues this way there is a possibility you will burn yourself out.");
+if (total>=55) { //total entered by user >=55 hours
+    console.log( total + " Hours is a lot of time to be taken out of the week and if continues this way there is a possibility you will burn yourself out.");//conditional answer for if
 }
-else{
-    console.log( total + " Hours is a comfortable amount of hour for the week so if given the opportunity you should study ahead and be prepared for upcoming class work.");
+else{ //total entered by user less than 55 hours
+    console.log( total + " Hours is a comfortable amount of hour for the week so if given the opportunity you should study ahead and be prepared for upcoming class work.");//conditional answer for else
 }
 
 console.log("The optimal amount of hours for work is about " + weekly + " so you do not burn yourself out and you get your work done with some personal time to spare.");
