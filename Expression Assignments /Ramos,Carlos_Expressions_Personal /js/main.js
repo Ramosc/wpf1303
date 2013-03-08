8b4f2eb03f062bd38f60b9531b70bcf41e9d70cf
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 10:36:41 -0500

    Expression personal 3 - time management and blocked prompts

--- Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -1,7 +1,9 @@
-var class1 = prompt("How many hours do you spend in WPF 1303 a week?");
-var class2 = prompt("How many hours do you spend in College Math a week?");
-var work   = prompt("How many hours do you spend at work each week?");
+//var class1 = prompt("How many hours do you spend in WPF 1303 a week?");
+//var class2 = prompt("How many hours do you spend in College Math a week?");
+//var work   = prompt("How many hours do you spend at work each week?");
 
-console.log("The workload at Full Sail can be very difficult at times.")
+console.log("Time management is very important at a school like Full Sail. The workload at Full Sail can be very difficult at times.");
+
+
 
 
