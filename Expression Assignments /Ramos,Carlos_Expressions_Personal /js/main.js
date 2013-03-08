8cfb8870dfac5043d0ec369c426ac631ee6f2aea
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-08 10:22:12 -0500

    Expression personal 1 - variables prompt work, class1, and class 2

---
+++ Expression Assignments /Ramos,Carlos_Expressions_Personal /js/main.js
@@ -0,0 +1,5 @@
+var class1 = prompt("How many hours do you spend in WPF 1303 a week?");
+var class2 = prompt("How many hours do you spend in College Math a week?");
+var work   = prompt("How many hours do you spend at work each week?");
+
+
