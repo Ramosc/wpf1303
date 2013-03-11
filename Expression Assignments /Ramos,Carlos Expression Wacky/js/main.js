9b82512f751fa11508d62934814951210746d170
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-11 09:12:54 -0400

    update all

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -12,4 +12,4 @@ console.log("Peter takes a different approach and buys a red rose for " + guy2 +
 console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");//transferred call
 console.log("The phone called is transferred and Peter hears Nun left behind Sister Crystal speaking how may I help you.");//crystal answers call
 console.log("Crystal recognizes Peter and says thank you for being such a nice guy and buying her a rose and thanks for calling today and donating to Nun left behind with your transfer.");//donation to nun left behind
-console.log("Peter realizes he is talking to a Nun and is not going to hook up with Crystal but has made a friend instead and between the two friends " + total + " dollars has been spent to find this out");// outcome/end of story 
+console.log("Peter realizes he is talking to a Nun and is not going to hook up with Crystal but has made a friend instead and between the two friends " + total + " dollars has been spent to find this out");// outcome/end of story
