3f5eef79ed672ae8a805be90e89006a724d48447
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 23:22:20 -0400

    expression wacky 18 - comment added crystal answers

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -10,6 +10,6 @@ console.log("They see this beautiful girl named Crystal at the bar that both guy
 console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");//mike buys a drink for crystal
 console.log("Peter takes a different approach and buys a red rose for " + guy2 + " dollars and Crystal hands him a card telling him to call her tomorrow.");//peter gets phone number
 console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");//transferred call
-console.log("The phone called is transferred and Peter hears Nun left behind Sister Crystal speaking how may I help you.");
+console.log("The phone called is transferred and Peter hears Nun left behind Sister Crystal speaking how may I help you.");//crystal answers call
 console.log("Crystal recognizes Peter and says thank you for being such a nice guy and buying her a rose and thanks for calling today and donating to Nun left behind with your transfer.");
 console.log("Peter realizes he is talking to a Nun and is not going to hook up with Crystal but has made a friend instead and between the two friends " + total + " dollars has been spent to find this out");
