7b38452356906497577d312f1bd3f0595cab4ea1
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 23:18:02 -0400

    expression wacky 15 - comment added for mike buying drink

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -7,7 +7,7 @@ var total = guy1 + guy2 + phone;//sum of the three var
 
 console.log("Two friends named Mike and Peter walk into a popular bar that they always hang out at.");//beginning the two friends story
 console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with.");//Crystal introduced
-console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");
+console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");//mike buys a drink for crystal 
 console.log("Peter takes a different approach and buys a red rose for " + guy2 + " dollars and Crystal hands him a card telling him to call her tomorrow.");
 console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");
 console.log("The phone called is transferred and Peter hears Nun left behind Sister Crystal speaking how may I help you.");
