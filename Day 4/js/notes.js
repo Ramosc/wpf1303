ed63fd950dc3fb970baf1f75c237be86dc639aba
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-11 15:26:29 -0400

    update before working on debugging

--- Day 4/js/notes.js
+++ Day 4/js/notes.js
@@ -1,18 +1,18 @@
 console.log("these are my notes");
 
-var phaser = parseInt(prompt("how many phasers?"));
+var phasers = parseInt(prompt("how many phasers?"));
 var setting = "stun";
 
-console.log("phasers:" + phaser);
+console.log("phasers:" + phasers);
 console.log("settings:" + setting);
 
-var atLeast3Phasers = (phaser > 3);
+var atLeast3Phasers = (phasers > 3);
 var setToKill = (setting == "kill");
 
 if (atLeast3Phasers || setToKill) {
     console.log("it's dead, jim.");
-}else if (setting == "stun") {
+}else if (setting == ("stun")){
     console.log("that won't be enough");
-}else {
+}else{
     console.log("run faster than the red shirt!");
 }
