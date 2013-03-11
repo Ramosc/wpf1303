b2a8848d87db57d974cd567bf5d492d98264b0e0
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 22:19:22 -0400

    expression wacky 4 - intro of crystal

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -1,6 +1,6 @@
-var guy1 = prompt("How much did Mike spend on Crystal?");
-var guy2 = prompt("How much did Peter spend on Crystal?");
+//var guy1 = prompt("How much did Mike spend on Crystal?");
+//var guy2 = prompt("How much did Peter spend on Crystal?");
 
 
-
-console.log("Two friends walk to a popular bar that they always hang out at.");
+console.log("Two friends named Mike and Peter walk into a popular bar that they always hang out at.");
+console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with");
