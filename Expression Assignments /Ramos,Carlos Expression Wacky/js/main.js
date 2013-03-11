92093b25fbb272eeea53bf534fe04435f29bf2cc
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 23:04:40 -0400

    expression wacky 11 - total spent end of story

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -1,6 +1,7 @@
 var guy1 = parseInt(prompt("How much did Mike spend on Crystal?"));
 var guy2 = parseInt(prompt("How much did Peter spend on Crystal?"));
 var phone = parseInt(prompt("How much did Peter give over the phone?"));
+var total = guy1 + guy2 + phone;
 
 
 
@@ -11,3 +12,4 @@ console.log("Peter takes a different approach and buys a red rose for " + guy2 +
 console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");
 console.log("The phone called is transferred and Peter hears Nun left behind Sister Crystal speaking how may I help you.");
 console.log("Crystal recognizes Peter and says thank you for being such a nice guy and buying her a rose and thanks for calling today and donating to Nun left behind with your transfer.");
+console.log("Peter realizes he is talking to a Nun and is not going to hook up with Crystal but has made a friend instead and between the two friends " + total + " dollars has been spent to find this out");
