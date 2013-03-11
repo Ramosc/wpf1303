2df6aa0d2ef2e84c98ab6497e2b2041c5e8c6c8a
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 22:25:16 -0400

    expression wacky 5 - mike buys a drink

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -1,6 +1,10 @@
-//var guy1 = prompt("How much did Mike spend on Crystal?");
+var guy1 = prompt("How much did Mike spend on Crystal?");
 //var guy2 = prompt("How much did Peter spend on Crystal?");
+
 
 
 console.log("Two friends named Mike and Peter walk into a popular bar that they always hang out at.");
 console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with");
+console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");
+
+
