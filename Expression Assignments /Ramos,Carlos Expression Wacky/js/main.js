328adff7f46cc3f52584295a8c20e0a782d6486e
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 22:39:59 -0400

    expression wacky 7 - peter calls number

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -1,5 +1,6 @@
-//var guy1 = prompt("How much did Mike spend on Crystal?");
-//var guy2 = prompt("How much did Peter spend on Crystal?");
+var guy1 = prompt("How much did Mike spend on Crystal?");
+var guy2 = prompt("How much did Peter spend on Crystal?");
+var  phone = prompt("How much did Peter give over the phone?");
 
 
 
@@ -7,4 +8,4 @@ console.log("Two friends named Mike and Peter walk into a popular bar that they 
 console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with");
 console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");
 console.log("Peter takes a different approach and buys a red rose for " + guy2 + " dollars and Crystal hands him a card telling him to call her tomorrow.");
-
+console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");
