86f0d96cc0784df4e1b151469752de20057df83f
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-11 12:28:27 -0400

    Day 4 - debugging

---
+++ Day 4/js/main.js
@@ -0,0 +1,14 @@
+console.log("cool.");
+
+var price = 5.99;
+
+console.log("price: " + price);
+
+var count = parseInt(prompt("how many"));
+
+console.log("price: " + price);
+
+var total = price * count;
+
+console.log("you owe me: $ " + total);
+
---
+++ Day 4/js/notes.js
@@ -0,0 +1,10 @@
+console.log("these are my notes");
+
+var phaser = parseInt(prompt("how many phasers do you have?"));
+var setting = "stun";
+
+if ((phaser>3) || (setting == "kill")) {
+    console.log("it's dead, jim.");
+} else {
+    console.log("run faster than the red shirt!");
+}
