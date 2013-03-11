9adaf2abd80c89e00747b273d8b22e23ec033b2a
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-10 23:11:14 -0400

    expression wacky 12 - comments added to var

--- Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
+++ Expression Assignments /Ramos,Carlos Expression Wacky/js/main.js
@@ -1,12 +1,12 @@
-var guy1 = parseInt(prompt("How much did Mike spend on Crystal?"));
-var guy2 = parseInt(prompt("How much did Peter spend on Crystal?"));
-var phone = parseInt(prompt("How much did Peter give over the phone?"));
-var total = guy1 + guy2 + phone;
+var guy1 = parseInt(prompt("How much did Mike spend on Crystal?"));//money spent by mike
+var guy2 = parseInt(prompt("How much did Peter spend on Crystal?"));//money spent by peter
+var phone = parseInt(prompt("How much did Peter give over the phone?"));//money spent by peter on the phone
+var total = guy1 + guy2 + phone;//sum of the three var 
 
 
 
 console.log("Two friends named Mike and Peter walk into a popular bar that they always hang out at.");
-console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with");
+console.log("They see this beautiful girl named Crystal at the bar that both guy would like hook up with.");
 console.log("Mike buys Crystal a drink for " + guy1 + " dollars and approach Crystal just to get shot down as just another player.");
 console.log("Peter takes a different approach and buys a red rose for " + guy2 + " dollars and Crystal hands him a card telling him to call her tomorrow.");
 console.log("Peter calls the number the next day and asks for Crystal but is told that a fee is required to be transferred so Peter pays " + phone + " dollars.");
