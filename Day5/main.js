f451898abbaf24577cd06abe18804b5e489266ec
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-13 10:22:42 -0400

    update day 5 - arrays

--- Day5/main.js
+++ Day5/main.js
@@ -9,10 +9,10 @@ console.log(firstGuy);
 var guyCount = names.length;// always one more than the last element
 console.log("There are " + guyCount + " guys in the back row.");
 
-var lastGuy = names[names.length - 1];
+var lastGuy = names[names.length - 1];//get the last element
 console.log("The last guy is " + lastGuy);
 
-var middleNumber = Math.floor(names.length / 2); // floor because arrays only understand whole numbers
-var middleGuy = names[middleNumber];
+var middleNumber = Math.floor(names.length / 2); // floor because arrays only
+var middleGuy = names[middleNumber]; // understand whole numbers
 console.log(middleGuy);
 
