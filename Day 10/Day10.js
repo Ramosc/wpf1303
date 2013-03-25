acca961bb8067511b283011ff951e8d76153735f
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-25 11:45:25 -0400

    completed cars exercise

--- Day 10/Day10.js
+++ Day 10/Day10.js
@@ -14,11 +14,24 @@ var choices = [{
     "miles" : 42000,
     "model" : 2009
 }];
+var distance = 30;
+var age = 10;
+var maintenance = 1000;
+var totalCost = function(totalMiles, age, maintenancePerYear, mpg) {
+    var gasDollars = mpg * totalMiles;
+    var maintenanceDollars = age * maintenancePerYear;
+    var tco = gasDollars + maintenanceDollars;
+    return  tco;
+};
+var totalDollars = totalCost(distance, age, maintenance, 15);
+console.log("This is the amount I can afford $" + totalDollars);
+
 var pickACar = function(cars) {
     var carNumber = 0;
     while (carNumber < cars.length){
         var car = cars[carNumber];
-        if (car.mpg >= 20 && car.miles <= 70000){
+        var totalDollars = totalCost(distance, age, maintenance, car.mpg);
+        if (car.mpg >= 20 && car.miles <= 70000 && totalDollars < 15000) {
             return car;
         }
         carNumber ++
@@ -26,4 +39,4 @@ var pickACar = function(cars) {
     return false;
 };
 var carSelected = pickACar(choices);
-console.log(carSelected);
+console.log("This is the car that fits my needs" , carSelected);
