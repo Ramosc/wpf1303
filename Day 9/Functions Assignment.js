64c222c13f00587617a2d152f8375f1bf74e380e
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-25 09:08:45 -0400

    functions assignment

--- Day 9/Functions Assignment.js
+++ Day 9/Functions Assignment.js
@@ -1,1 +1,14 @@
-alert("function assignment");
+var tickets = prompt("How much are movie tickets?");
+var entrance = prompt("How much is entrance into the club?");
+
+var date = function(moneySpent) {
+    return moneySpent * 2
+};
+var total = date(tickets);
+var total2 = date(entrance);
+if (total < total2) {
+    console.log("We should go to the movies because the cost of movie tickets is $" + total + " for two tickets");
+    } else (total > total2); {
+    console.log("We should go dancing because the cost to get into the club is $" + total2);
+}
+
