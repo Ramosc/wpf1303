23132a123972133ea614e503919abf6f08995e03
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 19:52:12 -0400

    industry 9 -book amount

--- Functions Assignment/industry/main.js
+++ Functions Assignment/industry/main.js
@@ -3,8 +3,8 @@ console.log("Industry Functions");
 
 var booksNeeded = 5;//books needed / ordered
 
-var learning = function(price) {// begin function 
-    var amountOfBooks = 0;
+var learning = function(price) {// begin function
+    var amountOfBooks = 0;//book amount
     while (amountOfBooks < booksNeeded) {
         var answer = "Do I need more books? " + ((amountOfBooks) ? " Yes I need more books!" : "No I have books");
         amountOfBooks++;
