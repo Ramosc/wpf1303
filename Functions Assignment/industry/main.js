1aa4d85062757cb83e332889e744d8f0ba5c2067
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 19:45:55 -0400

    industry 6 - console log trip amount

--- Functions Assignment/industry/main.js
+++ Functions Assignment/industry/main.js
@@ -8,10 +8,10 @@ var learning = function(price) {
     while (amountOfBooks < booksNeeded) {
         var answer = "Do I need more books? " + ((amountOfBooks) ? " Yes I need more books!" : "No I have books");
         amountOfBooks++;
-        
         console.log(answer);
     }
     return price * amountOfBooks;
 };
 var total = learning(10);
-console.log("The price for 5 book to learn web development will be " +  total);
+console.log("After " + booksNeeded + " trips im glad you finally enough books.");
+console.log("The price for " + booksNeeded  + " book to learn web development will be " +  total);
