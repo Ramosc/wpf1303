c0211815b64fc29394afcbfeb175ab87157cd46d
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 16:56:34 -0400

    functions wacky - test information for completion

--- Functions Assignment/Wacky/main.js
+++ Functions Assignment/Wacky/main.js
@@ -1,1 +1,26 @@
 console.log("Function Wacky");
+
+var gym = 400;
+var entree = 750;
+var soda = 200;
+var dailyIntake = 700;
+
+var calorieCount = function(entree, soda, gym) {
+    var calorieTotal = entree + soda;
+    var dailyCalorie = calorieTotal - gym;
+    return dailyCalorie;
+};
+var number = calorieCount(entree, soda, gym);
+console.log("I have taken in " + number + " calories after my gym session");
+
+if (number < dailyIntake) {
+    console.log("You need to continue working out to reach your goal.");
+} else {
+    console.log("Great job you have reached your goal, you burned " + number + " calories today.")
+}
+
+
+
+
+
+
