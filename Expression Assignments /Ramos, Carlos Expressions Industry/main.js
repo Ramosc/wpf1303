a9577beebe7bb3a74b017ac87e6c2b18128e63ec
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 14:36:31 -0400

    industry project  19 - comments added

--- Expression Assignments /Ramos, Carlos Expressions Industry/main.js
+++ Expression Assignments /Ramos, Carlos Expressions Industry/main.js
@@ -5,10 +5,10 @@ var teams = 2; // we have 2 teams
 var average = teamMembers * personalStyles * teams / teamMembers; // multiply members with styles then with team and divide by members get average
 
 
-console.log("The article I used for this project was from Smashing Magazine about Why Coding Styles Matters.");
-console.log("It discusses what coding style is and defines it as being a personal style of how your code looks");
-console.log("It also states that this can be very difficult in a team atmosphere because of all the different styles");
-console.log("It has a sample of code that uses comments to help illustrate how these can help while in a team");
+console.log("The article I used for this project was from Smashing Magazine about Why Coding Styles Matters.");//smashing mag article
+console.log("It discusses what coding style is and defines it as being a personal style of how your code looks");//personal styles
+console.log("It also states that this can be very difficult in a team atmosphere because of all the different styles");//team projects
+console.log("It has a sample of code that uses comments to help illustrate how these can help while in a team");//code shown
 console.log("While everyone has a personal coding style it is possible to work in teams and have a great project");
 console.log("If there are " + teams + " teams with " + teamMembers + " team members using " + personalStyles + " different editors being used");
 console.log("that can cause confusion because " + average + " different editors are being used but this can be resolved with great communication");
