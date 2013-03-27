ed32bf64b916f09fdc16333a2136da0fd0684ee6
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 17:15:09 -0400

    wacky function 11 - console log out answer

--- Functions Assignment/Wacky/main.js
+++ Functions Assignment/Wacky/main.js
@@ -13,7 +13,7 @@ var calorieCount = function(entree, soda, gym) {// begin function with three par
 var number = calorieCount(entree, soda, gym);//calling all calories
 console.log("I have taken in " + number + " calories after my gym session");// console out calories taken in
 if (number < dailyGoal) {//begin if statement
-    console.log("You need to continue working out to reach your goal.");
+    console.log("You need to continue working out to reach your goal.");// console log out answer
 } else {
     console.log("Great job you have reached your goal, you burned " + number + " calories today.")
 }
