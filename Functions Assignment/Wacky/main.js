d1e17e5f715497aa2f1ef169e865425a17272f36
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 17:14:17 -0400

    wacky function 10 - comment if statement

--- Functions Assignment/Wacky/main.js
+++ Functions Assignment/Wacky/main.js
@@ -3,7 +3,7 @@ console.log("Function Wacky");
 var gym = 400;
 var entree = 750;
 var soda = 200;
-var dailyIntake = 700;
+var dailyGoal = 500;
 
 var calorieCount = function(entree, soda, gym) {// begin function with three parameters
     var calorieTotal = entree + soda; // add parameters
@@ -12,8 +12,7 @@ var calorieCount = function(entree, soda, gym) {// begin function with three par
 };// end of function
 var number = calorieCount(entree, soda, gym);//calling all calories
 console.log("I have taken in " + number + " calories after my gym session");// console out calories taken in
-
-if (number < dailyIntake) {
+if (number < dailyGoal) {//begin if statement
     console.log("You need to continue working out to reach your goal.");
 } else {
     console.log("Great job you have reached your goal, you burned " + number + " calories today.")
