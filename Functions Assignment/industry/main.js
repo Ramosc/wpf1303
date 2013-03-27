f22e672eee0412bd3bf6718cfeac05c557da87ee
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 19:24:19 -0400

    industry 4 - ternary added

--- Functions Assignment/industry/main.js
+++ Functions Assignment/industry/main.js
@@ -1,9 +1,16 @@
 console.log("Industry Functions");
 
-var book = 1;
+
+var booksNeeded = 5;
 
 var learning = function(price) {
     var amountOfBooks = 0;
+    while (amountOfBooks < booksNeeded) {
+        var answer = "Do I need more books? " + ((amountOfBooks) ? " Yes I need more books!" : "No I have books");
+        amountOfBooks++;
+        console.log(answer);
+    }
     return price * amountOfBooks;
 };
-
+var total = learning(5);
+console.log(total);
