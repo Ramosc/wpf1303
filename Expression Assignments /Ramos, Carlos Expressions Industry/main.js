4eee8f96e8eccbc3a0d55f2c690058b2086961fd
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 13:52:11 -0400

    industry expression 6 - spelling correction

--- Expression Assignments /Ramos, Carlos Expressions Industry/main.js
+++ Expression Assignments /Ramos, Carlos Expressions Industry/main.js
@@ -5,4 +5,4 @@ console.log("Expression Industry");
 
 console.log("The article I used for this project was from Smashing Magazine about Why Coding Styles Matters.");
 console.log("It discusses what coding style is and defines it as being a personal style of how your code looks");
-console.log("It also states that this can be very difficult in a team atmosphere becasue of all the different styles");
+console.log("It also states that this can be very difficult in a team atmosphere because of all the different styles");
