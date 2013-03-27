6a70a06508dd2e21278cffa060718fb5945ea67d
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 13:57:58 -0400

    industry expression 8

--- Expression Assignments /Ramos, Carlos Expressions Industry/main.js
+++ Expression Assignments /Ramos, Carlos Expressions Industry/main.js
@@ -7,3 +7,4 @@ console.log("The article I used for this project was from Smashing Magazine abou
 console.log("It discusses what coding style is and defines it as being a personal style of how your code looks");
 console.log("It also states that this can be very difficult in a team atmosphere because of all the different styles");
 console.log("It has a sample of code that uses comments to help illustrate how these can help while in a team");
+console.log("While everyone has a personal coding style it is possible to work in teams and have a great project");
