44c78d8901b854b878e4405665779aa604ddbbed
Carlos Ramos <Ramosc@fullsail.edu>
2013-03-27 13:45:52 -0400

    industry expression 4

--- Expression Assignments /Ramos, Carlos Expressions Industry/main.js
+++ Expression Assignments /Ramos, Carlos Expressions Industry/main.js
@@ -4,3 +4,4 @@ console.log("Expression Industry");
 
 
 console.log("The article I used for this project was from Smashing Magazine about Why Coding Styles Matters.");
+console.log("It discusses what coding style is and defines it as being a personal style of how your code looks");
